e3f22fb6cc64cbcc2f3ff0d5e2e9203510242b10
David Mumford <David.Mumford@comcast.net>
2015-06-19 11:23:35 -0400

    Add the ability to display content when a marker on the map is clicked.

--- js/helper.js
+++ js/helper.js
@@ -158,16 +158,41 @@ function initializeMap() {
       title: name
     });
 
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>' + name + '</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
     // infoWindows are the little helper windows that open when you click
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: contentString //displayInfo
     });
 
-    // hmmmm, I wonder what this is about...
+    // hmmmm, I wonder what this is about... Glad that you asked!
+    // This morsel of code hooks up an event handler for markers on
+    // the map. When a marker is clicked, it will run the code in
+    // the anonymous function. We're going to open the infoWindow
+    // that's been created above.
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
